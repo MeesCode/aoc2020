cb64f0ab5d98699f8a3dfbc2ac9ef2c41ec7c905
MeesCode <m.brinkhuis@student.tudelft.nl>
2020-12-16 11:51:41 +0100

    finished day 13

--- day13/B.py
+++ day13/B.py
@@ -1,22 +1,21 @@
-import math
-
 f = open('data.txt').readlines()
 time = int(f[0][:-1])
-busses = [int(i) for i in f[1].split(',') if i != 'x']
+times = [i for i in f[1].split(',')]
+indexes = []
+busses = []
 
-def lcm(n):  
-    ans = 1
-    for i in n:  
-        ans = int((ans * i)/math.gcd(ans, i))          
-    return ans  
+for i in range(len(times)):
+    if times[i] != 'x':
+        busses.append(int(times[i]))
+        indexes.append(i)
 
-def find_offset(m, n, offset):
-    i = 1
+offset = 0
+cycle = 1
+
+for i in range(len(busses)):
     while True:
-        if i % m == 0 and (i + offset) % n == 0:
-            return i
-        i += 1
+        offset += cycle
+        if (offset + indexes[i]) % busses[i] == 0: break
+    cycle *= busses[i]
 
-offsets = [find_offset(busses[0], busses[i], i) for i in range(1, len(busses))]
-print(offsets)
-print(lcm(offsets))
+print('result: ', offset)
